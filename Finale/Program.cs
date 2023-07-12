7ec267b1809c03144bed8efabde13e0de56c2a1c
Anton Balashov <tyquat@gmail.com>
2023-07-12 13:00:12 +0300

    forth commit

---
+++ Finale/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] originalArray = { "Hello", "2", "world", ":-)" };
+        List<string> newList = new List<string>();
+
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            
+            if (originalArray[i].Length <= 3)
+            {
+                newList.Add(originalArray[i]);
+            }
+        }
+
+        string[] newArray = newList.ToArray();
+
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
